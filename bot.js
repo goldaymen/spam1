a75155b4bef0c9a432722f61c6ae43b8af343453
goldaymen <45092574+goldaymen@users.noreply.github.com>
2018-11-16 12:02:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "512912990748803072"; // ايدي السررفر
-var channel = "512913043487850506";//ايدي الروم
+var server = "512945089799258142"; // ايدي السررفر
+var channel = "512945137354276876";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
